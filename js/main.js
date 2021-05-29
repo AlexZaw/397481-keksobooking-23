a8143761b8e3121985f8c8d65186c9e0652a5a97
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-05-30 02:23:54 +0700

    fix: Поправь работу рандомайзера
    Распределение значений в функции getRandomNumber сделано более
    равномерным

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const getRandomNumber = (min, max, decimalPlaces = 0) => {
   min = min < 0 ? Math.abs(min) : min;
   max = max < 0 ? Math.abs(max) : max;
-  return +( Math.min(min, max) + Math.random() * Math.abs(max - min) ).toFixed(Math.abs(decimalPlaces));
+  return +(Math.abs( Math.min(min, max) - 0.5 + Math.random() * Math.abs(max - min + 1) ) ).toFixed(Math.abs(decimalPlaces) );
 };
 
 getRandomNumber(0, -10);

cf81abd962fe116bcd5fdbac6f3d12186a7a99c1
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-05-30 00:59:29 +0700

    refactor: объедини рандомайзеры в одну функцию

--- js/main.js
+++ js/main.js
@@ -1,25 +1,8 @@
-const getRandomIntNumber = (min, max) => {
+const getRandomNumber = (min, max, decimalPlaces = 0) => {
   min = min < 0 ? Math.abs(min) : min;
   max = max < 0 ? Math.abs(max) : max;
-  if(max < min){
-    const swap = max;
-    max = min;
-    min = swap;
-  }
-  return Math.floor(min + Math.random() * (max + 1 - min));
+  return +( Math.min(min, max) + Math.random() * Math.abs(max - min) ).toFixed(Math.abs(decimalPlaces));
 };
 
-const getRandomFloatNumber = (min, max, decimalPlaces) => {
-  min = min < 0 ? Math.abs(min) : min;
-  max = max < 0 ? Math.abs(max) : max;
-  if(max < min){
-    const swap = max;
-    max = min;
-    min = swap;
-  }
-  return +(min + Math.random() * (max - min)).toFixed(Math.abs(decimalPlaces));
-};
-
-
-getRandomIntNumber(0, -10);
-getRandomFloatNumber(0, -10, -3);
+getRandomNumber(0, -10);
+getRandomNumber(0, -10, -3);

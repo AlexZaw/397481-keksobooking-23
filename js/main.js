1a5e5fd08671aad249e1fbbae11a98db420da4f5
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-05-31 02:09:44 +0700

    refactor!: Перепиши код рандомайзера
    BREAKING CHANGE: функция рандомайзера больше не преобразовывает неверные
    значения

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const getRandomNumber = (min, max, decimalPlaces = 0) => {
-  min = min < 0 ? Math.abs(min) : min;
-  max = max < 0 ? Math.abs(max) : max;
-  let randomNumber;
-
-  if (decimalPlaces !== 0) {
-    randomNumber = (Math.min(min, max) + Math.random() * Math.abs(max - min)).toFixed(Math.abs(decimalPlaces));
+  let randomNumber = (min - 0.5) + Math.random() * (max - min + 1);
+  if (decimalPlaces === 0) {
+    randomNumber = Math.round(randomNumber);
   } else {
-    randomNumber = Math.floor(Math.min(min, max) + Math.random() * (Math.abs(max - min) + 1));
+    if (randomNumber < min) {
+      randomNumber += 0.5;
+    }
+    if (randomNumber > max) {
+      randomNumber -= 0.5;
+    }
+    randomNumber = randomNumber.toFixed(decimalPlaces);
   }
   return +randomNumber;
 };
 
-getRandomNumber(0, -10);
-getRandomNumber(0, -10, -3);
+getRandomNumber(1, 10);
+getRandomNumber(0, 10, 5);

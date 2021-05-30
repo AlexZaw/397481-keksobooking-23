a87d5e9371bcf6e5e81592054c09bc87c3b7eb21
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-05-30 21:31:26 +0700

    fix(randomizer): Исправь возвращаемые диапазоны
    Исправлена ошибка когда возвращаемое значение выходило за диапазон
    переданных значений

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const getRandomNumber = (min, max, decimalPlaces = 0) => {
   min = min < 0 ? Math.abs(min) : min;
   max = max < 0 ? Math.abs(max) : max;
-  return +(Math.abs( Math.min(min, max) - 0.5 + Math.random() * Math.abs(max - min + 1) ) ).toFixed(Math.abs(decimalPlaces) );
+  let randomNumber;
+
+  if (decimalPlaces !== 0) {
+    randomNumber = (Math.min(min, max) + Math.random() * Math.abs(max - min)).toFixed(Math.abs(decimalPlaces));
+  } else {
+    randomNumber = Math.floor(Math.min(min, max) + Math.random() * (Math.abs(max - min) + 1));
+  }
+  return +randomNumber;
 };
 
 getRandomNumber(0, -10);

27f0e151adaee45096768660659f567bd96a4b37
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-06-06 16:15:16 +0700

    refactor: перепиши рандомайзер

--- js/main.js
+++ js/main.js
@@ -70,32 +70,15 @@ const AvatarsOption = {
 };
 
 const getRandomNumber = (min, max, decimalPlaces = 0) => {
-  if (min < 0 || max < 0 || decimalPlaces < 0) {
-    throw new RangeError('Передаваемые значения должны быть больше или равны нулю');
-  }
-  if (min >= max) {
-    throw new Error('Минимальное значение не может быть больше или равно максимальному');
-  }
-  if (!min && min !==0 || !max && max !== 0) {
-    throw new Error('Диапазон передаваемых значений не определен');
-  }
-  if (typeof (min) !== 'number' || typeof (max) !== 'number' || typeof (decimalPlaces) !== 'number') {
-    throw new Error('Передаваемые значения должны быть числом');
-  }
-  let randomNumber = (min - 0.5) + Math.random() * (max - min + 1);
-  if (decimalPlaces === 0) {
-    randomNumber = Math.round(randomNumber);
-  } else {
-    if (randomNumber < min) {
-      randomNumber += 0.5;
-    }
-    if (randomNumber > max) {
-      randomNumber -= 0.5;
-    }
-    randomNumber = randomNumber.toFixed(decimalPlaces);
-  }
-  return +randomNumber;
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+  const result = decimalPlaces ?
+    (Math.random() * (upper - lower) + lower).toFixed(decimalPlaces) :
+    Math.floor(Math.random() * (upper - lower + 1) + lower);
+
+  return +result;
 };
+
 const getRandomArrayElement = (array) => array[getRandomNumber(0, array.length - 1)];
 
 const getRandomObjectMinMax = (obj) => getRandomNumber(obj.MIN, obj.MAX);
